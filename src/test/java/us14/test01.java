886eca7c78c3040631e3e1e585fcf9c9afa38780
aysel adela <adelaaysel@gmail.com>
2024-02-03 04:31:16 +0300

    aysel

--- src/test/java/us14/test01.java
+++ src/test/java/us14/test01.java
@@ -1,4 +1,22 @@
 package us14;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class test01 {
+    public static void main(String[] args) {
+
+
+        // WebDriver'ı başlat
+        WebDriver driver = new ChromeDriver();
+
+        // Web sayfasını yükleyin
+        driver.get("https://allovercommerce.com");
+
+
+
+        // Tarayıcıyı kapat
+        driver.quit();
+    }
 }
+
